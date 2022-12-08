ae491fe369a32e50bbf21de6cce94f74995629ea
Sam Platt <platts1@sou.edu>
2022-12-07 17:12:28 -0800

    overhaul of code, add and list work now

--- todo/main.go
+++ todo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -11,47 +12,85 @@ type task struct {
 	complete bool
 }
 
-var todolist []task
-
 func main() {
 
-	if len(os.Args) <= 1 {
-		fmt.Println("no arguments detected")
+	validcommands := []string{
+		"add", "complete", "delete", "help", "list", "quit",
 	}
 
-	command := os.Args[1]
+	var todolist []task
 
-	newtask := task{
-		name:     strings.Join(os.Args[2:], " "),
-		complete: false,
+	var command, taskname string
+
+	for command != "quit" {
+		fmt.Printf("\nEnter a command: ")
+		fmt.Scanln(&command)
+
+		if command == "add" {
+			fmt.Printf("\nEnter the name of the task to add it to your to do list: ")
+			scanner := bufio.NewScanner(os.Stdin)
+			if scanner.Scan() {
+				taskname = scanner.Text()
+			}
+
+			index := search(todolist, taskname)
+			if index != -1 {
+				fmt.Printf("\nThere is already a task with the name %s in your todo list. Consider using the command complete or delete instead", taskname)
+			} else {
+				todolist = add(task{name: taskname, complete: false})
+			}
+
+			list()
+		} else if command == "complete" {
+			fmt.Printf("\nEnter the name of the task to complete it")
+			fmt.Scanln(&taskname)
+
+			index := search(todolist, taskname)
+			if index == -1 {
+				fmt.Printf("\nA task with the name %s was not found. Consider adding this task to your todo list or confirming the correct taskname", taskname)
+			} else {
+				todolist[index].complete = true
+			}
+			list()
+		} else if command == "delete" {
+			fmt.Printf("\nEnter the name of the task to delete it")
+			fmt.Scanln(&taskname)
+
+			index := search(todolist, taskname)
+			if index == -1 {
+				fmt.Printf("\nA task with the name %s was not found. Consider adding this task to your todo list or confirming the correct taskname", taskname)
+			} else {
+				delete(index)
+			}
+			list()
+		} else if command == "help" {
+			fmt.Printf("\nValid commands are: %s", strings.Join(validcommands, ", "))
+		} else if command == "list" {
+			list()
+		} else if command == "quit" {
+			return
+		} else {
+			fmt.Printf("\nCommand not found. Consider using the help command to list all valid commands\n")
+			return
+		}
+
 	}
-
-	switch command {
-	case "add":
-		add(newtask)
-	case "complete":
-		complete(newtask)
-	case "delete":
-		delete(newtask)
-	case "list":
-		list()
-	case "quit":
-		os.Exit(0)
-	case "help":
-		help()
-	}
-
 }
 
-func help() {
-	// TODO: list all commands
-}
 func list() {
-	fmt.Println(todolist)
+	fmt.Println("TODO LIST:")
+	for _, x := range todolist {
+		var status string
+		if x.complete {
+			status = "x"
+		} else {
+			status = " "
+		}
+		fmt.Printf("[%s] %s", status, x.name)
+	}
 }
-func add(t task) {
-	todolist = append(todolist, t)
-	list()
+func add(t task) []task {
+	return append(todolist, t)
 }
 func complete(t task) {
 	for i, x := range todolist {
@@ -62,12 +101,16 @@ func complete(t task) {
 	}
 	list()
 }
-func delete(t task) {
-	for _, x := range todolist {
-		if x.name == t.name {
-			// TODO: delete element
-			return
+func delete(i int) []task {
+	return append(todolist[:i], todolist[i+1:]...)
+}
+
+/* Returns index of task if found, returns -1 if not found */
+func search(tasks []task, name string) int {
+	for i, x := range todolist {
+		if x.name == name {
+			return i
 		}
 	}
-	list()
+	return -1
 }
